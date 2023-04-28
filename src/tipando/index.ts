a183a0aacbea024e5697aa962e24ea0ff2b76e82
DioguinhoSantos <diiogosantos@live.com>
2023-04-28 16:28:59 -0300

    starting typescript

--- src/tipando/index.ts
+++ src/tipando/index.ts
@@ -1,10 +1,10 @@
-function buscarCarrosPorMarca(frota, marca) {
+function buscarCarrosPorMarca(frota: {}[], marca: string) {
   if (marca === undefined) {
     return frota
   }
 
   return frota.filter(
-    (carro) => {
+    (carro: {}) => {
       return carro.marca === marca
     }
   )
